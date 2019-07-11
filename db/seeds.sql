53e566e0fedbe76077ec267ef852470475559fe6
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-07-11 12:14:46 -0500

    minimum seeds.sql information added

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,13 +2,32 @@ USE initiative_db;
 
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
-  `email` VARCHAR (30)
-, `password` VARCHAR (20)
+  `email` VARCHAR (50)
+, `password` VARCHAR (50)
 , `admin` BOOLEAN
+);
+
+DROP TABLE IF EXISTS Games;
+CREATE TABLE Games (
+  `name` VARCHAR (250)
+);
+
+DROP TABLE IF EXISTS Characters;
+CREATE TABLE Characters (
+  `name` VARCHAR (250)
+, `armor-class` INT
+, `hit-points` INT
+, `image` VARCHAR (250)
 );
 
 INSERT INTO Users (`email`, `password`, `admin`)
 VALUES
 ('jon@gmail.com', 'password', 1);
 
-SELECT * FROM Users;
+INSERT INTO Games (`name`)
+VALUES
+('My First D&D Game');
+
+INSERT INTO Characters (`name`, `armor-class`, `hit-points`, `image`)
+VALUES
+('Ront', 17, 68, './images/orc.png');
