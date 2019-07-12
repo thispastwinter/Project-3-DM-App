32ff35694758238bceb63dd247f97fc61c722f0f
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-07-11 21:30:58 -0500

    seeds.sql fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,35 @@
+-- USE initiative_db;
+
+-- DROP TABLE IF EXISTS Users;
+-- CREATE TABLE Users (
+--   `id` INT AUTO_INCREMENT NOT NULL
+-- , `email` VARCHAR (50)
+-- , `password` VARCHAR (50)
+-- , `admin` BOOLEAN
+-- , PRIMARY KEY(`id`)
+-- );
+
+-- DROP TABLE IF EXISTS Games;
+-- CREATE TABLE Games (
+--   `id` INT AUTO_INCREMENT NOT NULL
+-- , `name` VARCHAR (250)
+-- , PRIMARY KEY(`id`)
+-- );
+
+-- DROP TABLE IF EXISTS Characters;
+-- CREATE TABLE Characters (
+--   `id` INT AUTO_INCREMENT NOT NULL
+-- , `name` VARCHAR (250)
+-- , `initiative` INT
+-- , `armorClass` INT
+-- , `hitPoints` INT
+-- , `image` VARCHAR (250)
+-- , PRIMARY KEY(`id`)
+-- );
+
+-- =======================================================================
+
 USE initiative_db;
-
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users (
-  `id` INT AUTO_INCREMENT NOT NULL
-, `email` VARCHAR (50)
-, `password` VARCHAR (50)
-, `admin` BOOLEAN
-, PRIMARY KEY(`id`)
-);
-
-DROP TABLE IF EXISTS Games;
-CREATE TABLE Games (
-  `id` INT AUTO_INCREMENT NOT NULL
-, `name` VARCHAR (250)
-, PRIMARY KEY(`id`)
-);
-
-DROP TABLE IF EXISTS Characters;
-CREATE TABLE Characters (
-  `id` INT AUTO_INCREMENT NOT NULL
-, `name` VARCHAR (250)
-, `initiative` INT
-, `armorClass` INT
-, `hitPoints` INT
-, `image` VARCHAR (250)
-, PRIMARY KEY(`id`)
-);
 
 INSERT INTO Users (`email`, `password`, `admin`, createdAt, updatedAt)
 VALUES
