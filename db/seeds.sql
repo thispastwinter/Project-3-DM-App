e6bd657c01a07ce0022a53617262a9b248ff5e98
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-07-12 08:41:05 -0500

    undid changes from last night from package.json/seeds.sql/and the models

--- db/seeds.sql
+++ db/seeds.sql
@@ -31,14 +31,14 @@
 
 USE initiative_db;
 
-INSERT INTO Users (`email`, `password`, `admin`, createdAt, updatedAt)
+INSERT INTO Users (`email`, `password`, `admin`, created_at, updated_at)
 VALUES
 ('jon@gmail.com', 'password', 1, CURDATE(), CURDATE());
 
-INSERT INTO Games (`name`, createdAt, updatedAt, UserId)
+INSERT INTO Games (`name`, created_at, updated_at, user_id)
 VALUES
 ('My First D&D Game', CURDATE(), CURDATE(), 1);
 
-INSERT INTO Characters (`name`, `initiative`, `armorClass`, `hitPoints`, `image`, createdAt, updatedAt, GameId)
+INSERT INTO Characters (`name`, `initiative`, `armor_class`, `hit_points`, `image`, created_at, updated_at, game_id)
 VALUES
 ('Ront', 12, 17, 68, './images/orc.png', CURDATE(), CURDATE(), 1);
