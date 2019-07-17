d7e1cc292c26eee55f650de3c33e562def37f5be
Tyler Ward <T.ward26@gmail.com>
2019-07-17 18:08:01 -0500

    App now pulling data from DB

---
+++ client/src/components/initPage/index.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import List from '../list/index';
+import ListItem from '../listItem/index';
+import axios from 'axios';
+import socketIOClient from 'socket.io-client';
+
+class InitPage extends Component {
+    state = {
+        characterList: [],
+        endpoint: "localhost:3001"
+    }
+
+
+    componentDidMount() {
+        this.loadChars()
+        console.log(this.state.characterList);
+
+        window.addEventListener("beforeunload", this.onUnload);
+        const stateObject = JSON.parse(localStorage.getItem("state"));
+        this.setState(stateObject);
+
+        const socket = socketIOClient(this.state.endpoint);
+        socket.on('listChange', (characterList) => {
+            console.log('Change received');
+            this.setState({ characterList });
+        });
+    }
+
+    loadChars = () => {
+        axios.get('/api/v1/characters')
+            .then(res => {
+                let characterList = res.data;
+                console.log(characterList);
+                this.send(this.setState({ characterList }));
+            });
+    };
+
+    onUnload = (event) => {
+        localStorage.setItem("state", JSON.stringify(this.state));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("beforeunload", this.onUnload)
+    }
+
+    send = async (func) => {
+        await func
+        const socket = socketIOClient(this.state.endpoint);
+        socket.emit('listChange', this.state.characterList)
+        console.log('Sending List Change');
+    }
+
+    initSort = (array) => {
+        const newArr = array.slice()
+        newArr.sort((a, b) => {
+            return b.initiative - a.initiative;
+        });
+        this.send(this.setState((state) => {
+            return { characterList: newArr };
+        }));
+    }
+
+    turnDone = (id) => {
+        const characterList = this.state.characterList.slice();
+        characterList.push(...characterList.splice(characterList.findIndex(c => c.id === id), 1))
+        this.send(this.setState({ characterList }));
+    }
+
+    editChar = (updatedCharacter) => {
+        console.log(updatedCharacter);
+        axios.post('/api/v1/characters', {
+            hit_points: updatedCharacter.hit_points,
+            initiative: updatedCharacter.initiative,
+            id: updatedCharacter.id
+        })
+            .then(res => {
+                console.log(res);
+            });
+        this.send(this.setState({
+            characterList: this.state.characterList
+                .map((character) => character.id === updatedCharacter.id ? updatedCharacter : character)
+        }));
+    }
+
+    removeChar = (id) => {
+        const characterList = this.state.characterList.slice();
+        characterList.splice(characterList.findIndex(c => c.id === id), 1);
+        this.send(this.setState({ characterList }));
+    }
+
+    resetEncounter = () => {
+        const characterList = this.state.characterList.slice();
+        characterList.map(obj => {
+            return obj.initiative = parseInt(0);
+        });
+        this.send(this.setState({ characterList }));
+    }
+
+    render() {
+        return (
+            <div>
+                <List >
+                    {this.state.characterList.map(character => (
+                        <ListItem
+                            character={character}
+                            id={character.id}
+                            key={character.id}
+                            ac={character.armor_class}
+                            init={character.initiative}
+                            name={character.name}
+                            health={character.hit_points}
+                            turnDone={this.turnDone}
+                            editInit={this.editChar}
+                            editHealth={this.editChar}
+                            removeChar={this.removeChar}
+                            currentOrder={this.state.characterList}
+                        />
+                    ))}
+                </List>
+                <button onClick={this.resetEncounter}>Reset Encounter</button>
+                <button onClick={() => this.initSort(this.state.characterList)}>Initiative Sort</button>
+            </div>
+        )
+    }
+}
+
+export default InitPage;
