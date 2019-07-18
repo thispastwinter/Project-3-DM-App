9d6be8cf56b68468481ddbf6fa574280e9c5b67f
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2019-07-18 17:00:45 -0500

    created all new routes for hue lights

--- routes/api/hue_lights.js
+++ routes/api/hue_lights.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const hueLightsController = require('../../controllers/huelights');
 
+router.route('/detect')
+  .post(hueLightsController.detect);
 router.route('/connect')
-  .post(hueLightsController.connect)
+  .post(hueLightsController.connect);
+router.route('/alllights')
+  .post(hueLightsController.allLights);
+router.route('/controllights')
+  .post(hueLightsController.controlLights);
 
 module.exports = router;
--- routes/api/huelights.js
+++ routes/api/huelights.js
@@ -44,45 +44,7 @@ const hueLights = () => {
   // Once this connection is established, requests can then be made to trigger light events.
 
   app.post('/lights', (req, res) => {
-    const lightState = hue.lightState;
-    let host = req.body.host;
-    let username = req.body.username;
-    let light = req.body.light;
-    let api = new HueApi(host, username);
-    let state;
-    switch (req.body.huestate) {
-      case 'on':
-        state = lightState.create().on();
-        api.setLightState(light, state, function (err, lights) {
-          if (err) throw err;
-          res.json(lights)
-        });
-        break;
-      case 'off':
-        state = lightState.create().off();
-        api.setLightState(light, state, function (err, lights) {
-          if (err) throw err;
-          res.json(lights)
-        });
-        break;
-      case 'critical':
-        state = lightState.create().longAlert();
-        api.setLightState(light, state, function (err, lights) {
-          if (err) throw err;
-          res.json(lights)
-        });
-        break;
-      case 'lightning':
-        lightning(api, light);
-        break;
-      default:
-        state = lightState.create().on();
-        api.setLightState(light, state, function (err, lights) {
-          if (err) throw err;
-          res.json(lights)
-        });
-    };
-  });
+    
 };
 
 module.exports = hueLights;
