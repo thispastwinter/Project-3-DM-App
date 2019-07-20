21ad63de89300fbf853450462a3888c31c359a01
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-07-20 10:37:10 -0500

    pulling dev

--- client/src/components/loginPage/index.js
+++ client/src/components/loginPage/index.js
@@ -9,6 +9,7 @@ class LoginPage extends Component {
     this.state = {
       email: '',
       password: '',
+      loginSuccess: false,
     };
 
     this.handleLogin = this.handleLogin.bind(this);
@@ -24,8 +25,7 @@ class LoginPage extends Component {
         email: this.state.email,
         password: this.state.password
       });
-      console.log('RESPONSE.DATA', response.data);
-      console.log('RESPONSE.DATA.PASSWORD', response.data.password);
+      // ????????????????????????????? if email and password match, this.setState loginSuccess: true
     } catch (err) {
       if (err) throw err;
     }
