dd934bcc544b5d9a277fe34a8a2f6141d6ff0f9c
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2019-07-20 15:00:09 -0500

    finished implementing pseudocode

--- controllers/huelights.js
+++ controllers/huelights.js
@@ -3,6 +3,24 @@ const HueApi = require('node-hue-api').HueApi;
 const express = require('express');
 require('../light_effects/lightning');
 
+// Remote Pseudocode //
+
+// >GET https://api.meethue.com/oauth2/auth?clientid=<clientid>&appid=<appid>&deviceid=<deviceid>&devicename=<devicename>&state=<state>&response_type=code
+
+// This get request is made, and redirects the user to the hue login page. It then asks them if they give permission for the app to use their acct.
+// The user is then redirected and a code is received from as a reponse.
+// This code will be given to the second request and used to trigger a 401 with a valid nonce key.
+// Another request is made with the given header from Hue Developer Remote Page implementing the clientid, nonce key, and a calculated hashed response.
+
+// I.E:
+
+//HASH1	MD5(“CLIENTID” + “:” + “REALM” + “:” + “CLIENTSECRET”) //clientId and secret will be stored in a .env
+//HASH2	MD5(“VERB” + “:” + “PATH”)
+//response	MD5(HASH1 + “:” + “NONCE” + “:” + HASH2)
+// let HASH1 = MD5("kVWjgzqk8hayM38pAudrA6psflju6k0T:oauth2_client@api.meethue.com:GHFV3f4L736bwgEB");
+// let HASH2 = MD5("POST:/oauth2/token");
+// let response = MD5(HASH1 + ":" + "7b6e45de18ac4ee452ee0a0de91dbb10" + ":" + HASH2);
+
 const detect = (req, res) => {
   hue.nupnpSearch(function (err, result) {
     if (err) throw err;
