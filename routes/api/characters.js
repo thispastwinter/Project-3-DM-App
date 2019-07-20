11a1e576229931b56c9893ace2fc25c659f1bb49
Tyler Ward <T.ward26@gmail.com>
2019-07-19 21:17:54 -0500

    Update characters.js

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -4,8 +4,9 @@ const characterController = require('../../controllers/characters');
 // http://localhost:3001/api/v1/characters
 router.route('/')
   .get(characterController.findAll)
-  .post(characterController.create)
+  .post(characterController.updateChar);
+  //.post(characterController.create)
   // .delete(characterController.destroy);
-//   .post(characterController.updateChar)
+
 
 module.exports = router;
