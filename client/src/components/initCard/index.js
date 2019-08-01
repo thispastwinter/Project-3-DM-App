479ec11140407b3b9b4465ecd4ccafd271506235
Tyler Ward <T.ward26@gmail.com>
2019-08-01 18:34:36 -0500

    Update index.js

--- client/src/components/initCard/index.js
+++ client/src/components/initCard/index.js
@@ -63,7 +63,7 @@ const InitCard = (props) => {
     else {
       let index = currentOrder.findIndex(c => c.id === props.id)
       return (
-        <Card className="init-card">
+        <Card className={index === 0 ? 'init-blinking' : 'init-card'}>
           <Card.Content>
             <Media>
               <Media.Item position="left">
